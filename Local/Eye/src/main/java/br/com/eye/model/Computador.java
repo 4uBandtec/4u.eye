21131a83e604cdea06c55d177a5267e847b22fc0
LiviaSayuri <livia.sayuri@hotmail.com>
2018-10-23 00:04:08 -0300

    Reparo Get e Set

--- Local/Eye/src/main/java/br/com/eye/model/Computador.java
+++ Local/Eye/src/main/java/br/com/eye/model/Computador.java
@@ -1,101 +1,135 @@
 package br.com.eye.model;
 
 import br.com.eye.controller.ControllerComputador;
-import oshi.hardware.CentralProcessor;
-import oshi.software.os.OperatingSystem;
+import br.com.eye.dao.StatementComputador;
+
+import java.sql.SQLException;
 
 public class Computador {
 
-    private int codComputador;
+    private Integer codComputador;
     private String nome;
-    OperatingSystem sistemaOperacional;
+    private String sistemaOperacional;
     private String versaoSistema;
-    private int versaoBits;
-    CentralProcessor processador;
-    private long totalCpu;
+    private Integer versaoBits;
+    private String processador;
+    private Double totalCpu;
     private Double totalDisco;
-    private Double totalMemoria;
-    private int codUsuario;
+    private long totalMemoria;
+    private Integer codUsuario;
 
     ControllerComputador controllerComputador = new ControllerComputador();
+    StatementComputador statementComputador = new StatementComputador();
 
-    public int getCodComputador() {
-        return codComputador;
+    public Computador() {
     }
 
-    public void setCodComputador(int codComputador) {
+    public Computador(Integer codComputador, String nome, String sistemaOperacional, String versaoSistema, Integer versaoBits, String processador, Double totalCpu, Double totalDisco, long totalMemoria, Integer codUsuario) {
         this.codComputador = codComputador;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
         this.nome = nome;
+        this.sistemaOperacional = sistemaOperacional;
+        this.versaoSistema = versaoSistema;
+        this.versaoBits = versaoBits;
+        this.processador = processador;
+        this.totalCpu = totalCpu;
+        this.totalDisco = totalDisco;
+        this.totalMemoria = totalMemoria;
+        this.codUsuario = codUsuario;
     }
 
-    public OperatingSystem getSistemaOperacional() {
+    public Computador(int aInt, String string, String string0, String string1, int aInt0, String string2, long aLong, double aDouble, double aDouble0, double aDouble1, int codUsuario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Integer getCodComputador(int codigoUsuario) throws SQLException {
+        return statementComputador.getCodComputador(codigoUsuario);
+    }
+
+    public String getNome(Computador computador) throws SQLException {
+        return statementComputador.getNome(computador.codComputador);
+    }
+
+    public String getSistemaOperacional() {
         return controllerComputador.getSistemaOperacional();
     }
 
-    public void setSistemaOperacional(String sistemaOperacional) {
-        //this.sistemaOperacional = sistemaOperacional;
-    }
-
     public String getVersaoSistema() {
-        return versaoSistema;
-    }
-
-    public void setVersaoSistema(String versaoSistema) {
-        this.versaoSistema = versaoSistema;
+        return controllerComputador.getVersaoSistema();
     }
 
     public int getVersaoBits() {
         return controllerComputador.getBit();
     }
 
-    public void setVersaoBits(int versaoBits) {
-        this.versaoBits = versaoBits;
-    }
-
-    public CentralProcessor getProcessador() {
+    public String getProcessador() {
         return controllerComputador.getProcessador();
     }
 
-    public void setProcessador(String processador) {
-        //this.processador = processador;
-    }
-
-    public double getTotalCpu() {
+    public Double getTotalCpu() {
         return controllerComputador.getCPU();
-    }
-
-    public void setTotalCpu(Double totalCpu) {
-
     }
 
     public Double getTotalDisco() {
         return totalDisco;
     }
 
-    public void setTotalDisco(Double totalDisco) {
-        this.totalDisco = totalDisco;
-    }
-
     public long getTotalMemoria() {
         return controllerComputador.getMemoria();
-    }
-
-    public void setTotalMemoria(Double totalMemoria) {
-        this.totalMemoria = totalMemoria;
     }
 
     public int getCodUsuario() {
         return codUsuario;
     }
 
-    public void setCodUsuario(int codUsuario) {
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodComputador(Integer codComputador) {
+        this.codComputador = codComputador;
+    }
+
+    public void setSistemaOperacional(String sistemaOperacional) {
+        this.sistemaOperacional = sistemaOperacional;
+    }
+
+    public void setVersaoSistema(String versaoSistema) {
+        this.versaoSistema = versaoSistema;
+    }
+
+    public void setVersaoBits(Integer versaoBits) {
+        this.versaoBits = versaoBits;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
+    public void setTotalCpu(Double totalCpu) {
+        this.totalCpu = totalCpu;
+    }
+
+    public void setTotalDisco(Double totalDisco) {
+        this.totalDisco = totalDisco;
+    }
+
+    public void setTotalMemoria(long totalMemoria) {
+        this.totalMemoria = totalMemoria;
+    }
+
+    public void setCodUsuario(Integer codUsuario) {
         this.codUsuario = codUsuario;
+    }
+
+    public Computador getComputadorSalvo() throws SQLException {
+        return statementComputador.getComputadorSalvo(1);
+    }
+
+    public Computador getComputadorAtual() throws SQLException {
+        return controllerComputador.getComputadorAtual(1);
+    }
+
+    public boolean verificaAtualizacao(Computador atual, Computador salvo) {
+        return true;//Arumar metodo verificaAtualizacao();
     }
 }
