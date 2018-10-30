ec28410b96bb3f4a3fdc92a241b9b16c988979a4
henrique-gs <henriquegodez@gmail.com>
2018-10-30 15:33:15 -0300

    Modificações no estilo da dashboard

---
+++ Web/Eye/Eye/Controllers/EYEDashStyle.js
@@ -0,0 +1,42 @@
+﻿
+var x;
+var y;
+
+function getCoordenadas() {
+    x = event.clientX;
+    y = event.clientY;
+    //var coords = "X coords: " + x + ", Y coords: " + y;
+    //console.log(coords);
+}
+
+function iniciar() {
+    getItemMenu();
+}
+
+function getItemMenu() {
+
+    var itemMenuBackgrounds = document.getElementsByClassName("itemMenuBackGround");
+    var itemMenus = document.getElementsByClassName("itemMenu");
+
+    for (var i = 0; i < itemMenus.length; ++i) {
+        var item = itemMenus[i];
+        var itembg = itemMenuBackgrounds[i];
+
+        itemMenus[i].addEventListener("mousemove", function () { efeitoHover(itembg, item) });
+        itemMenus[i].addEventListener("mouseout", function () { efeitoOut(itembg, item) });
+        
+    }
+    
+}
+
+
+
+function efeitoHover(itembgs, items) {
+    var width = items.getBoundingClientRect().width;
+    var backgroundX = (-width / 2) + (x - width / 2);
+    itembgs.style.left = backgroundX + "px";
+}
+
+function efeitoOut(itembgs, items) {
+    itembgs.style.left = "-200%";
+}
