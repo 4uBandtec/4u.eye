35c4f42ead2dc9fde40227957b82027cd486c1e7
LiviaSayuri <livia.sayuri@hotmail.com>
2018-10-29 22:39:07 -0300

    Retira codUsuario do metodo que obtem computadorAtual

--- Local/Eye/src/main/java/br/com/eye/controller/ControllerComputador.java
+++ Local/Eye/src/main/java/br/com/eye/controller/ControllerComputador.java
@@ -1,6 +1,6 @@
 package br.com.eye.controller;
+
 import br.com.eye.model.Computador;
-import java.sql.SQLException;
 import oshi.SystemInfo;
 import oshi.software.os.FileSystem;
 import oshi.software.os.OSFileStore;
@@ -8,16 +8,15 @@ import oshi.software.os.OSFileStore;
 public class ControllerComputador {
 
     SystemInfo systemInfo = new SystemInfo();
-    
+
     FileSystem fileSystem = systemInfo.getOperatingSystem().getFileSystem();
 
-    public Computador getComputadorAtual(int codUsuario) throws SQLException {
+    public Computador getComputadorAtual() {
         return new Computador(
                 getSistemaOperacional(),
                 getVersaoSistema(),
                 getBit(),
                 getProcessador(),
-                getCPU(),
                 getDisco(),
                 getMemoria()
         );
@@ -29,7 +28,6 @@ public class ControllerComputador {
 
     public String getVersaoSistema() {
         return systemInfo.getOperatingSystem().getVersion().toString();
-
     }
 
     public int getBit() {
@@ -41,32 +39,21 @@ public class ControllerComputador {
     }
 
     public Double getCPU() {
-        
-        return systemInfo.getHardware().getProcessor().getSystemCpuLoad()*100.0;
+        return systemInfo.getHardware().getProcessor().getSystemCpuLoad() * 100.0;
     }
 
     public long getDisco() {
-       
         OSFileStore[] fsArray = fileSystem.getFileStores();
-        
-        
         long total = 0;
-        
+
         for (OSFileStore fs : fsArray) {
 
             total += fs.getTotalSpace();
-
         }
-        
-           return total;
+        return total;
     }
 
     public long getMemoria() {
         return systemInfo.getHardware().getMemory().getTotal();
     }
-
-    public boolean verificaAtualizacao(Computador atual, Computador salvo) {
-        return atual == salvo;//Falta completar
-    }
-
 }
