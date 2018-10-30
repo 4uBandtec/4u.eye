152df48ff938feb3c6f64bd81f8c191f78008e28
henrique-gs <henriquegodez@gmail.com>
2018-10-29 22:22:52 -0200

    Inicio da criação do estilo da dashboard

---
+++ Web/Eye/Eye/Controllers/EYEDash.js
@@ -0,0 +1,17 @@
+﻿ google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['', ''],
+          ['Usada',     11],
+          ['Livre',      2]
+        ]);
+
+        var options = {
+            pieHole: 0.9,
+            backgroundColor: { fill: 'transparent' }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
