e448c3874bf41584a7a14868385185f90c900fa0
LiviaSayuri <livia.sayuri@hotmail.com>
2018-10-29 22:30:47 -0300

    Retira espaços em branco e arruma identação

--- Local/Eye/src/main/java/br/com/eye/controller/ControllerLeituraComputador.java
+++ Local/Eye/src/main/java/br/com/eye/controller/ControllerLeituraComputador.java
@@ -1,4 +1,3 @@
-
 package br.com.eye.controller;
 
 import oshi.SystemInfo;
@@ -8,37 +7,29 @@ import oshi.software.os.FileSystem;
 import oshi.software.os.OSFileStore;
 
 public class ControllerLeituraComputador {
-    
-    
-    
+
     SystemInfo systemInfo = new SystemInfo();
     HardwareAbstractionLayer hardware = systemInfo.getHardware();
     CentralProcessor processor = hardware.getProcessor();
     FileSystem fileSystem = systemInfo.getOperatingSystem().getFileSystem();
-    
-    
-    public double getCPUUsada(){
+
+    public double getCPUUsada() {
         Double cl = processor.getSystemCpuLoad();
-        return cl*100.0;
+        return cl * 100.0;
     }
-    
-    public double getMemoriaDisponivel(){
+
+    public double getMemoriaDisponivel() {
         return systemInfo.getHardware().getMemory().getAvailable();
     }
-    
-    public long getDiscoDisponivel(){
+
+    public long getDiscoDisponivel() {
         OSFileStore[] fsArray = fileSystem.getFileStores();
-        
-        
+
         long disponivel = 0;
-        
+
         for (OSFileStore fs : fsArray) {
-
             disponivel += fs.getUsableSpace();
-
         }
-        
-           return disponivel;
+        return disponivel;
     }
-    
 }
