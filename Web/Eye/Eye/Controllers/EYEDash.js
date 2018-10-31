1e42dd89891fdcda531365733f7730388a391859
henrique-gs <github@alunos.bandtec.com.br>
2018-10-30 22:04:47 -0200

    Avanço no estilo da dashboard

--- Web/Eye/Eye/Controllers/EYEDash.js
+++ Web/Eye/Eye/Controllers/EYEDash.js
@@ -10,9 +10,12 @@
         var options = {
             pieHole: 0.9,
             backgroundColor: { fill: 'rgb(16,16,16)' },
-            colors: ['rgb(226,26,47)', 'rgb(16,16,16)']
-        };
+            colors: ['rgb(226,26,47)', 'rgb(16,16,16)'],
+            pieSliceBorderColor: "transparent"
 
+
+        };
+          
         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
         chart.draw(data, options);
       }
