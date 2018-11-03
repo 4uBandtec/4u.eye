9766f8ed8b513844b73c5563d3b7ab61d9310dd6
LiviaSayuri <livia.sayuri@hotmail.com>
2018-11-03 19:25:16 -0300

    Adiciona metodo de cadastro

--- Web/Eye/Eye/Model/Workspace.cs
+++ Web/Eye/Eye/Model/Workspace.cs
@@ -5,14 +5,29 @@ namespace Eye.Model
 {
     public class Workspace
     {
-        private int CodWorkspace { get; set; }
-        private string Workspacename { get; set; }
-        private string Senha { get; set; }
-        private string Salt { get; set; }
+        public int CodWorkspace { get; set; }
+        public string Workspacename { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public int Salt { get; set; }
+
         public bool Logar(TextBox txtWorkspacename, TextBox txtSenha)
         {
             Valida.StringVazia(txtWorkspacename, txtSenha);
-            return (new ControllerWorkspace().AutenticaWorkspace(txtWorkspacename.Text, txtSenha.Text));
+            return (new ControllerWorkspace().AutenticarWorkspace(txtWorkspacename.Text, txtSenha.Text));
+        }
+        public bool Cadastrar(TextBox txtWorkspacename, TextBox txtNome, TextBox txtEmail, TextBox txtSenha)
+        {
+            Valida.StringVazia(txtWorkspacename, txtNome, txtEmail, txtSenha);
+            Valida.Email(txtEmail);
+            var workspace = new Workspace();
+            workspace.Workspacename = txtWorkspacename.Text;
+            workspace.Nome = txtNome.Text;
+            workspace.Email = txtEmail.Text;
+            workspace.Senha = txtSenha.Text;
+
+            return (new ControllerWorkspace().Cadastrar(workspace));
         }
         public int GetCodigo(string workspace)
         {
@@ -20,3 +35,4 @@ namespace Eye.Model
         }
     }
 }
+
