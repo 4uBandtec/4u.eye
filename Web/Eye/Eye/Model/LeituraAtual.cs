5e696b00cff91f96d5dfa93247de0a563935b90d
LiviaSayuri <livia.sayuri@hotmail.com>
2018-11-06 20:17:27 -0200

    Adiciona query de leitura atual

---
+++ Web/Eye/Eye/Model/LeituraAtual.cs
@@ -0,0 +1,19 @@
+﻿namespace Eye.Model
+{
+    public class LeituraAtual
+    {
+        //public long RAM { get; set; }
+        //public long CPU { get; set; }
+        //public long Disco { get; set; }
+    }
+    //public double GetPorcentagemRAM()
+    //{
+
+    //}
+    //public double GetPorcentagemCPU()
+    //{
+    //}
+    //public double GetPorcentagemDisco()
+    //{
+    //}
+}
