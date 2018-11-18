64ae07e2fb1d4f0262baf1b2b78be44db0af2cb4
LiviaSayuri <livia.sayuri@hotmail.com>
2018-11-18 16:51:08 -0200

    Adiciona calculo de perfil

---
+++ Web/EYE/EYE/Model/Enum/TiposDePerfil.cs
@@ -0,0 +1,15 @@
+﻿namespace EYE.Model
+{
+	public partial class Perfil
+	{
+		public enum TiposDePerfil
+		{
+			Indefinido = 0,
+			Jogo = 1,
+			Trabalho = 2,
+			Social = 3,
+			Outros = 4
+		}
+	}
+
+}
---
+++ Web/EYE/EYE/Model/Perfil.cs
@@ -0,0 +1,42 @@
+﻿namespace EYE.Model
+{
+	public partial class Perfil
+	{
+		private int jogo;
+		private int trabalho;
+		private int social;
+		private int outros;
+
+		public int Jogo { get => jogo; set => jogo = value; }
+		public int Trabalho { get => trabalho; set => trabalho = value; }
+		public int Social { get => social; set => social = value; }
+		public int Outros { get => outros; set => outros = value; }
+		public TiposDePerfil CalculaPerfilUsuario(Perfil perfil)
+		{
+			var maior = 0;
+			var tipoPerfil = TiposDePerfil.Indefinido;
+			if (perfil.Jogo > maior)
+			{
+				maior = perfil.Jogo;
+				tipoPerfil = TiposDePerfil.Jogo;
+			}
+			if (perfil.Trabalho > maior)
+			{
+				maior = perfil.Trabalho;
+				tipoPerfil = TiposDePerfil.Trabalho;
+			}
+			if (perfil.Social > maior)
+			{
+				maior = perfil.Social;
+				tipoPerfil = TiposDePerfil.Social;
+			}
+			if (perfil.Outros > maior)
+			{
+				maior = perfil.Outros;
+				tipoPerfil = TiposDePerfil.Outros;
+			}
+			return tipoPerfil;
+		}
+	}
+
+}
