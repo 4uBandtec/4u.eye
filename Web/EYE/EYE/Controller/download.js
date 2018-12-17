cd2283695eba5f4d6b0a58b19cd271900a14f724
henrique-gs <henriquegodez@gmail.com>
2018-12-17 14:09:40 -0200

    Implenta visualização das cores no popup

--- Web/EYE/EYE/Controller/download.js
+++ Web/EYE/EYE/Controller/download.js
@@ -1,4 +1,4 @@
 ﻿function baixar() {
-    window.location.href = "https://4ueye.file.core.windows.net/eye/4uEye.jar?sv=2018-03-28&ss=bqtf&srt=sco&sp=rwdlacup&se=2018-12-17T08:19:46Z&sig=XXQWrnh8URmYT9%2FiwdKBFQ6UDtHgZAvG8tkTA3QSimM%3D&_=1545005995664";
+    window.location.href = "https://github.com/4uBandtec/4u.eye/raw/develop/Local/Eye/target/Eye-1.0-jar-with-dependencies.jar";
 }
 
