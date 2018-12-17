745c2e8310f25c029c3a82e927fc789eb9414bbe
Livia Sayuri <livia.sayuri@hotmail.com>
2018-12-16 23:50:44 -0200

    Adiciona metodo que alterna a mensagem da notificação

---
+++ Web/EYE/EYE/Model/Enum/TipoMensagem.cs
@@ -0,0 +1,12 @@
+﻿namespace EYE.Model.Enum
+{
+	public enum TipoMensagem
+	{
+		CadastroUsuario = 1,
+		CadastroTarefa = 2,
+		CadastroSlack = 3,
+		CPUElevada = 4,
+		RAMElevada = 5,
+		HDElevado = 6
+	}
+}
