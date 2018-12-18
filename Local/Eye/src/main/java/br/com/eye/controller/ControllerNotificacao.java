da28c3da9fa08162f7bfbba152ec9b16a7d11b23
Livia Sayuri <livia.sayuri@hotmail.com>
2018-12-18 01:38:41 -0200

    Adiciona notificação local e atualiza statement slack

--- Local/Eye/src/main/java/br/com/eye/controller/ControllerNotificacao.java
+++ Local/Eye/src/main/java/br/com/eye/controller/ControllerNotificacao.java
@@ -1,23 +1,33 @@
 package br.com.eye.controller;
 
 import br.com.eye.model.LogMensagem;
+import br.com.eye.model.Notificacao;
 import br.com.eye.model.SlackMensagem;
 import br.com.eye.model.dao.StatementNotificacao;
 import br.com.eye.model.dao.StatementSlack;
+import java.awt.AWTException;
+import java.awt.TrayIcon;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class ControllerNotificacao {
 
     public static void EnviaSlack(int codWorkspace) throws SQLException, IOException {
-        String mensagem = StatementNotificacao.getMensagemParaEnviarSlack(codWorkspace);
-        if (mensagem != null) {
-            SlackMensagem slack = StatementSlack.getSlack(codWorkspace);
-            if (slack.getUrl().length() > 0 && slack.getCanal().length() > 0) {
+        SlackMensagem slack = StatementSlack.getSlack(codWorkspace);
+        if (slack.getUrl().length() > 0) {
+            String mensagem = StatementNotificacao.getMensagemParaEnviarSlack(codWorkspace);
+            if (mensagem != null) {
                 SlackMensagem.EnviaMensagem(slack, mensagem);
             }
         }
-        LogMensagem.GravarLog("Notificação enviada para o Slack");
+    }
+
+    public static void EnviaLocal(int codUsuario) throws SQLException, IOException, AWTException {
+        String mensagem = StatementNotificacao.getMensagemParaEnviarLocal(codUsuario);
+        if (mensagem != null) {
+            new Notificacao().construirNotificacaoLocal(mensagem, TrayIcon.MessageType.NONE);
+        }
+
     }
 
 }
