7e66ffee9dd40f8cc64589293dc737557f80678f
Livia Sayuri <livia.sayuri@hotmail.com>
2018-12-18 02:30:55 -0200

    Adiciona Incremento de minuto caso tarefa ativa e processo sendo executado

--- Local/Eye/src/main/java/br/com/eye/model/dao/StatementTarefa.java
+++ Local/Eye/src/main/java/br/com/eye/model/dao/StatementTarefa.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.eye.model.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- *
- * @author henri
- */
 public class StatementTarefa {
-    
-    public static boolean ativaTarefa(int codTarefa, int codProcesso, int codUsuario) throws SQLException{
+
+    public static boolean ativaTarefa(int codTarefa, int codProcesso, int codUsuario) throws SQLException {
         String sql = "UPDATE processo_tarefa set ativa = 1 WHERE cod_tarefa = ? AND cod_processo = ? AND cod_usuario = ?";
         PreparedStatement query = new Conexao().getConexao().prepareStatement(sql);
         query.setInt(1, codTarefa);
@@ -22,11 +14,27 @@ public class StatementTarefa {
         query.setInt(3, codUsuario);
         return query.execute();
     }
-    
-    public static boolean desativaTarefas(int codUsuario) throws SQLException{
+
+    public static boolean desativaTarefas(int codUsuario) throws SQLException {
         String sql = "UPDATE processo_tarefa set ativa = 0 WHERE cod_usuario = ?";
         PreparedStatement query = new Conexao().getConexao().prepareStatement(sql);
         query.setInt(1, codUsuario);
+        return query.execute();
+    }
+
+    public static String retonaProcessoTarefaAtiva(int codUsuario) throws SQLException {
+        String sql = "SELECT nome_processo, nome_aplicacao FROM processo WHERE cod_processo IN (SELECT cod_processo FROM processo_tarefa WHERE cod_usuario = ? AND ativa = 1) ";
+        PreparedStatement query = new Conexao().getConexao().prepareStatement(sql);
+        query.setInt(1, codUsuario);
+        ResultSet resultado = query.executeQuery();
+        return resultado.next() ? resultado.getString("nome_processo") : null;
+    }
+
+    public static boolean AdicionaMinuto(int codUsuario) throws SQLException {       
+        String sql = "UPDATE processo_tarefa set minutos_feitos = ((SELECT minutos_feitos where cod_usuario = ? AND ativa = 1) + 1)WHERE cod_usuario = ? AND ativa = 1";
+        PreparedStatement query = new Conexao().getConexao().prepareStatement(sql);
+        query.setInt(1, codUsuario);
+        query.setInt(2, codUsuario);
         return query.execute();
     }
 }
